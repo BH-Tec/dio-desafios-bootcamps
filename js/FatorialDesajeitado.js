d5e20d700145c6488d3977dca3dffa38565c5262
Bruno Henrique <brunodorea@outlook.com.br>
2023-02-10 09:47:12 -0300

    ♻️ refactor: correção do código para o desafio Fatorial Desajeitado em JavaScript

--- js/FatorialDesajeitado.js
+++ js/FatorialDesajeitado.js
@@ -1,5 +1,9 @@
 // Desafio
-// O fatorial de um número inteiro positivo N é o produto de todos os inteiros positivos menores ou iguais a n. Fazemos um fatorial desajeitado usando os inteiros em ordem decrescente, trocando as operações de multiplicação por uma rotação fixa de operações cuja ordem é: multiplicar '*', dividir '/', adicionar '+' e subtrair '-'. Por exemplo, desajeitado(10) = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1. Lembre-se que no fatorial desajeitado as operações ainda são aplicadas usando a ordem usual de operações aritméticas. Além disso, a divisão neste caso sempre resulta em um número inteiro, por exemplo, 90 / 8 = 11. Dado um inteiro N, retorne o fatorial desajeitado de n.
+// O fatorial de um número inteiro positivo N é o produto de todos os inteiros positivos menores ou iguais a n.
+// Fazemos um fatorial desajeitado usando os inteiros em ordem decrescente, trocando as operações de multiplicação por uma rotação fixa de operações cuja ordem é:
+// multiplicar '*', dividir '/', adicionar '+' e subtrair '-'. Por exemplo, desajeitado(10) = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1.
+// Lembre-se que no fatorial desajeitado as operações ainda são aplicadas usando a ordem usual de operações aritméticas.
+// Além disso, a divisão neste caso sempre resulta em um número inteiro, por exemplo, 90 / 8 = 11. Dado um inteiro N, retorne o fatorial desajeitado de n.
 
 // Entrada
 // A entrada consiste em um valor inteiro positivo N.
@@ -7,35 +11,55 @@
 // Saída
 // A saída consiste em retornar o valor do seu fatorial desajeitado de N.
 
-let lines = gets().split("\n")
-let n = parseInt(lines.shift())
+let lines = gets()
+let valor = lines
+let cont = 1
+let ns = []
 
-if (n <= 2) {
-  n * 1
-  print(n)
-} else {
-  let res = 0
-  let cnt = 1
-  let tmp = n
-  n--
-  while (n > 0) {
-    tmp *= n
-    n--
-    if (n != 0) {
-      tmp /= n;
-      n--
+let mat1 = "Math.floor("
+let mat2 = ")"
+let cmat = false
+
+function fatorial(n) {
+  for (let i = n; i > 0; i--) {
+    if (cont === 1 && i > 1) {
+      ns.push(mat1)
+      cmat = true
+      ns.push(n)
+      ns.push("*")
+      cont = 2
     }
-    tmp += n
-    n--
 
-    res += tmp
+    else if (cont === 2 && i > 1) {
+      ns.push(n)
+      ns.push("/")
+      cont = 3
+    }
 
-    tmp = -n
-    if (n == 1) {
-      res += tmp
+    else if (cont === 3 && i > 1) {
+      ns.push(n)
+      ns.push(mat2)
+      cmat = false
+      ns.push("+")
+      cont = 4
+    }
+
+    else if (cont === 4 && i > 1) {
+      ns.push(n)
+      ns.push("-")
+      cont = 1
     }
 
     n--
   }
-  print(res.toFixed(0))
-} 
+
+  ns.push(1)
+  if (cmat === true) {
+    ns.push(mat2)
+  }
+
+  let juntos = ns.join("")
+  print(eval(juntos))
+}
+
+fatorial(valor)
